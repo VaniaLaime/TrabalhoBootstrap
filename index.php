39d7ffd145a803afcab19d59d31d5c03e2850c00
SoulEaterAlban <brenovinioli@gmail.com>
2019-02-27 22:00:48 -0300

    adicionei um nav e cards

--- index.php
+++ index.php
@@ -12,13 +12,132 @@
     <title>Olá, mundo!</title>
   </head>
   <body>
-    <h1>Olá, mundo!</h1>
-
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+ <a class="navbar-brand" href="#">
+    <img src="images/Logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
+    Exercicios
+  </a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Alterna navegação">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav">
+      <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(Página atual)</span></a>
+        
+      <a class="nav-item nav-link" href="#">Exercicio1</a>
+      <a class="nav-item nav-link" href="#">Exercicio2</a>
+      <a class="nav-item nav-link" href="#">Exercicio3</a>
+      <a class="nav-item nav-link" href="#">Exercicio4</a>
+      <a class="nav-item nav-link" href="#">Exercicio5</a>
+      <a class="nav-item nav-link" href="#">Exercicio6</a>
+      <a class="nav-item nav-link" href="#">Exercicio7</a>
+      <a class="nav-item nav-link" href="#">Exercicio8</a>  
+      <a class="nav-item nav-link" href="#">Exercicio9</a> 
+        
+        
+    </div>
+  </div>
+</nav>
+    <section>
       
-    <!-- JavaScript (Opcional) -->
-    <!-- jQuery primeiro, depois Popper.js, depois Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+      <div class="container-fluid">
+        <div class="card-deck container-fluid">
+            
+        <div class="card" style="width: 18rem;">
+        <img class="card-img-top" src="images/code.jpg" alt="Imagem de capa do card">
+        <div class="card-body">
+        <h5 class="card-title">Título do card</h5>
+        <p class="card-text">Um exemplo de texto rápido para construir o título do card e fazer  preencher o conteúdo do card.</p>
+        <a href="#" class="btn btn-primary">Visitar</a>
+    </div>
+          </div>
+            
+            <div class="card" style="width: 18rem;">
+        <img class="card-img-top" src="images/code.jpg" alt="Imagem de capa do card">
+        <div class="card-body">
+        <h5 class="card-title">Título do card</h5>
+        <p class="card-text">Um exemplo de texto rápido para construir o título do card e fazer  preencher o conteúdo do card.</p>
+        <a href="#" class="btn btn-primary">Visitar</a>
+    </div>
+          </div>
+            
+            
+            <div class="card" style="width: 18rem;">
+        <img class="card-img-top" src="images/code.jpg" alt="Imagem de capa do card">
+        <div class="card-body">
+        <h5 class="card-title">Título do card</h5>
+        <p class="card-text">Um exemplo de texto rápido para construir o título do card e fazer  preencher o conteúdo do card.</p>
+        <a href="#" class="btn btn-primary">Visitar</a>
+    </div>
+          </div>
+            
+                <div class="card" style="width: 18rem;">
+        <img class="card-img-top" src="images/code.jpg" alt="Imagem de capa do card">
+        <div class="card-body">
+        <h5 class="card-title">Título do card</h5>
+        <p class="card-text">Um exemplo de texto rápido para construir o título do card e fazer  preencher o conteúdo do card.</p>
+        <a href="#" class="btn btn-primary">Visitar</a>
+    </div>
+          </div>
+    
+          
+                <div class="card" style="width: 18rem;">
+        <img class="card-img-top" src="images/code.jpg" alt="Imagem de capa do card">
+        <div class="card-body">
+        <h5 class="card-title">Título do card</h5>
+        <p class="card-text">Um exemplo de texto rápido para construir o título do card e fazer  preencher o conteúdo do card.</p>
+        <a href="#" class="btn btn-primary">Visitar</a>
+    </div>
+          </div>
+            
+    </div>        
+            <div class="card-deck container">
+                <div class="card" style="width: 18rem;">
+        <img class="card-img-top" src="images/code.jpg" alt="Imagem de capa do card">
+        <div class="card-body">
+        <h5 class="card-title">Título do card</h5>
+        <p class="card-text">Um exemplo de texto rápido para construir o título do card e fazer  preencher o conteúdo do card.</p>
+        <a href="#" class="btn btn-primary">Visitar</a>
+    </div>
+          </div>
+            
+                <div class="card" style="width: 18rem;">
+        <img class="card-img-top" src="images/code.jpg" alt="Imagem de capa do card">
+        <div class="card-body">
+        <h5 class="card-title">Título do card</h5>
+        <p class="card-text">Um exemplo de texto rápido para construir o título do card e fazer  preencher o conteúdo do card.</p>
+        <a href="#" class="btn btn-primary">Visitar</a>
+    </div>
+          </div>
+            
+                <div class="card" style="width: 18rem;">
+        <img class="card-img-top" src="images/code.jpg" alt="Imagem de capa do card">
+        <div class="card-body">
+        <h5 class="card-title">Título do card</h5>
+        <p class="card-text">Um exemplo de texto rápido para construir o título do card e fazer  preencher o conteúdo do card.</p>
+        <a href="#" class="btn btn-primary">Visitar</a>
+    </div>
+          </div>
+            
+                <div class="card" style="width: 18rem;">
+        <img class="card-img-top" src="images/code.jpg" alt="Imagem de capa do card">
+        <div class="card-body">
+        <h5 class="card-title">Título do card</h5>
+        <p class="card-text">Um exemplo de texto rápido para construir o título do card e fazer  preencher o conteúdo do card.</p>
+        <a href="#" class="btn btn-primary">Visitar</a>
+    </div>
+          </div>
+            
+            
+    </div>
+        
+        </div>
+    </section>  
+    
+      
+      
+      <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
   </body>
 </html>
