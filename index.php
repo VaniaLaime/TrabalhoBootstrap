e048e66b295667f63f26c5069b03ad32a31d5448
SoulEaterAlban <brenovinioli@gmail.com>
2019-02-28 22:31:37 -0300

    adc a formatação dos cards

--- index.php
+++ index.php
@@ -40,10 +40,10 @@
 </nav>
     <section>
       
-      <div class="container-fluid">
-        <div class="card-deck container-fluid">
+      <div class="container center" style="margin-top:5rem;">
+        <div class="card-deck" style="margin-bottom:2rem;">
             
-        <div class="card" style="width: 18rem;">
+        <div class="card" style="width: 14rem;">
         <img class="card-img-top" src="images/code.jpg" alt="Imagem de capa do card">
         <div class="card-body">
         <h5 class="card-title">Título do card</h5>
@@ -52,7 +52,7 @@
     </div>
           </div>
             
-            <div class="card" style="width: 18rem;">
+            <div class="card" style="width: 14rem;">
         <img class="card-img-top" src="images/code.jpg" alt="Imagem de capa do card">
         <div class="card-body">
         <h5 class="card-title">Título do card</h5>
@@ -62,7 +62,7 @@
           </div>
             
             
-            <div class="card" style="width: 18rem;">
+            <div class="card" style="width: 14rem;">
         <img class="card-img-top" src="images/code.jpg" alt="Imagem de capa do card">
         <div class="card-body">
         <h5 class="card-title">Título do card</h5>
@@ -71,7 +71,7 @@
     </div>
           </div>
             
-                <div class="card" style="width: 18rem;">
+                <div class="card" style="width: 14rem;">
         <img class="card-img-top" src="images/code.jpg" alt="Imagem de capa do card">
         <div class="card-body">
         <h5 class="card-title">Título do card</h5>
@@ -79,9 +79,48 @@
         <a href="#" class="btn btn-primary">Visitar</a>
     </div>
           </div>
-    
+    </div> 
+           <div class="card-deck" style="margin-bottom:2rem;" >
+                <div class="card" style="width: 14rem;">
+        <img class="card-img-top" src="images/code.jpg" alt="Imagem de capa do card">
+        <div class="card-body">
+        <h5 class="card-title">Título do card</h5>
+        <p class="card-text">Um exemplo de texto rápido para construir o título do card e fazer  preencher o conteúdo do card.</p>
+        <a href="#" class="btn btn-primary">Visitar</a>
+    </div>
+          </div>
+            
+           
+           
+                <div class="card" style="width: 14rem;">
+        <img class="card-img-top" src="images/code.jpg" alt="Imagem de capa do card">
+        <div class="card-body">
+        <h5 class="card-title">Título do card</h5>
+        <p class="card-text">Um exemplo de texto rápido para construir o título do card e fazer  preencher o conteúdo do card.</p>
+        <a href="#" class="btn btn-primary">Visitar</a>
+    </div>
+          </div>
+            
+                <div class="card" style="width: 14rem;">
+        <img class="card-img-top" src="images/code.jpg" alt="Imagem de capa do card">
+        <div class="card-body">
+        <h5 class="card-title">Título do card</h5>
+        <p class="card-text">Um exemplo de texto rápido para construir o título do card e fazer  preencher o conteúdo do card.</p>
+        <a href="#" class="btn btn-primary">Visitar</a>
+    </div>
+          </div>
+            
+                <div class="card" style="width: 14rem;">
+        <img class="card-img-top" src="images/code.jpg" alt="Imagem de capa do card">
+        <div class="card-body">
+        <h5 class="card-title">Título do card</h5>
+        <p class="card-text">Um exemplo de texto rápido para construir o título do card e fazer  preencher o conteúdo do card.</p>
+        <a href="#" class="btn btn-primary">Visitar</a>
+    </div>
+          </div>
+            </div>
           
-                <div class="card" style="width: 18rem;">
+                <div class="card" style="width: 16rem;">
         <img class="card-img-top" src="images/code.jpg" alt="Imagem de capa do card">
         <div class="card-body">
         <h5 class="card-title">Título do card</h5>
@@ -90,54 +129,22 @@
     </div>
           </div>
             
-    </div>        
-            <div class="card-deck container">
-                <div class="card" style="width: 18rem;">
-        <img class="card-img-top" src="images/code.jpg" alt="Imagem de capa do card">
-        <div class="card-body">
-        <h5 class="card-title">Título do card</h5>
-        <p class="card-text">Um exemplo de texto rápido para construir o título do card e fazer  preencher o conteúdo do card.</p>
-        <a href="#" class="btn btn-primary">Visitar</a>
-    </div>
-          </div>
-            
-                <div class="card" style="width: 18rem;">
-        <img class="card-img-top" src="images/code.jpg" alt="Imagem de capa do card">
-        <div class="card-body">
-        <h5 class="card-title">Título do card</h5>
-        <p class="card-text">Um exemplo de texto rápido para construir o título do card e fazer  preencher o conteúdo do card.</p>
-        <a href="#" class="btn btn-primary">Visitar</a>
-    </div>
-          </div>
-            
-                <div class="card" style="width: 18rem;">
-        <img class="card-img-top" src="images/code.jpg" alt="Imagem de capa do card">
-        <div class="card-body">
-        <h5 class="card-title">Título do card</h5>
-        <p class="card-text">Um exemplo de texto rápido para construir o título do card e fazer  preencher o conteúdo do card.</p>
-        <a href="#" class="btn btn-primary">Visitar</a>
-    </div>
-          </div>
-            
-                <div class="card" style="width: 18rem;">
-        <img class="card-img-top" src="images/code.jpg" alt="Imagem de capa do card">
-        <div class="card-body">
-        <h5 class="card-title">Título do card</h5>
-        <p class="card-text">Um exemplo de texto rápido para construir o título do card e fazer  preencher o conteúdo do card.</p>
-        <a href="#" class="btn btn-primary">Visitar</a>
-    </div>
-          </div>
-            
-            
+          
     </div>
         
-        </div>
+        
     </section>  
-    
-      
-      
       <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>    
+
+    <footer>
+
+        <p class="pargh">&copy; Direitos reservados, Breno , James e Vania </p>
+
+      </footer>
+ 
+      
+  
   </body>
 </html>
