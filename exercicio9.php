04104f53ede62618e3dd66e98b5607abd24d89b7
VaniaLaime <vania.laime2002@gmail.com>
2019-03-06 17:09:34 -0300

    adicionei o exercicio 9

---
+++ exercicio9.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <!-- Meta tags Obrigatórias -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+      <link rel="stylesheet" href="css/style.css">
+
+    <title>Exercicio9 !</title>
+  </head>
+  <body>
+       <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+          <a class="navbar-brand" href="#">
+             <img src="images/Logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
+              Exercicios
+            </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Alterna navegação">
+            <span class="navbar-toggler-icon"></span>
+         </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+       <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(Página atual)</span></a>
+        
+      <a class="nav-item nav-link" href="#">Exercicio1</a>
+      <a class="nav-item nav-link" href="#">Exercicio2</a>
+      <a class="nav-item nav-link" href="#">Exercicio3</a>
+      <a class="nav-item nav-link" href="#">Exercicio4</a>
+      <a class="nav-item nav-link" href="#">Exercicio5</a>
+      <a class="nav-item nav-link" href="#">Exercicio6</a>
+      <a class="nav-item nav-link" href="#">Exercicio7</a>
+      <a class="nav-item nav-link" href="#">Exercicio8</a>  
+      <a class="nav-item nav-link" href="#">Exercicio9</a> 
+        
+        
+    </div>
+  </div>
+</nav>
+      
+      
+      <!-- exercico 3-->
+   <div class="container center">
+      <h1 class="display-4 text-muted">exercicio 9</h1>
+      <h2>Multiplos de 3 do 1 ao 200 </h2>
+   
+      
+      <?php
+        //echo "<h1 class='titulo' > exercicio 3 </h1>"
+         
+      $Multiplos;
+
+      for ($n=1; $n<=200 ; $n++) { 
+        # code...
+         $Multiplos=$n%3;
+         
+        if ($Multiplos==0){
+          echo "$n  é multiplo de 3 </br>";
+        }
+      }
+      
+
+      ?> 
+    </div>
+    <!-- JavaScript (Opcional) -->
+    <!-- jQuery primeiro, depois Popper.js, depois Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  </body>
+</html>
